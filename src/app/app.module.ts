beca5ebc44f6bf5c5b8b0b3ef9c218be0e6dba40
Arpit <arpit.goel@nagarro.com>
2020-07-23 01:18:56 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsContentComponent } from './news-content/news-content.component';
-import { SiteHeaderComponent } from './site-header/site-header.component';
-import { SiteFooterComponent } from './site-footer/site-footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment.prod';
 @NgModule({
   declarations: [
     AppComponent,
-    NewsContentComponent,
-    SiteHeaderComponent,
-    SiteFooterComponent
+    NewsContentComponent
   ],
   imports: [
     BrowserModule,
