7923d8dc455b22a47f69c64ea97a3d1ff53d1350
Arpit <arpit.goel@nagarro.com>
2020-07-24 01:54:22 +0530

    changes

--- src/app/news-content/news-content.component.spec.ts
+++ src/app/news-content/news-content.component.spec.ts
@@ -36,7 +36,7 @@ describe('NewsContentComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create  ', () => {
     expect(component).toBeTruthy();
   });
 });
